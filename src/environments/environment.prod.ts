d14f4899e1583a201b6d82ad202b8a6f0012f626
oscarpon <oscar.pontebaquero@gmail.com>
2022-01-08 01:51:30 +0100

    config production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  authUrl:'http://localhost:5000/auth/',
+  accountUrl: 'http://localhost:5000/accounts/',
+  hospitalUrl: 'http://localhost:5000/hospital/',
+  buildingUrl: 'http://localhost:5000/buildings/',
+  wareHouseUrl: 'http://localhost:5000/warehouse/'
 };
